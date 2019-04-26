70b0264da94b1bf7dfc837be65f2572780c55845
sylvainonweb <sylvain.onweb@gmail.com>
2019-04-26 14:37:52 +0200

    1er commit (suite)

---
+++ DotNetCoreWebApiTests/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreWebApiTests.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreWebApiTests.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET api/customer
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            IList<Customer> customers = new List<Customer>();
+            for(int i = 1; i <= 10; i++)
+            {
+                customers.Add(new Customer { Id = i, LastName = "Nom " + i, FirstName = "Prénom " + i });
+            }
+
+            return customers.ToArray();
+        }
+    }
+}
---
+++ DotNetCoreWebApiTests/Data/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreWebApiTests.Data
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
