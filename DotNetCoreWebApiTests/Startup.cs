174ed3774a4ceeca230cb90e1e5f2c01e4bd426b
sylvainonweb <sylvain.onweb@gmail.com>
2019-04-29 16:28:15 +0200

    Fix #2 : mise en place de swagger

--- DotNetCoreWebApiTests/Startup.cs
+++ DotNetCoreWebApiTests/Startup.cs
@@ -61,11 +61,11 @@ namespace DotNetCoreWebApiTests
 
             // AJOUT SBD
             app.UseMiddleware<LoggingMiddleware>();
-            app.UseSwagger(); // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();       // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwaggerUI(c => 
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API");
-            }); // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            });                     // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
 
             app.UseEndpoints(endpoints =>
             {
